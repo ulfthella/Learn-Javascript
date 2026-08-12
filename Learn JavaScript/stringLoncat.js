7bf682e23399541b1e9307e4f7e542e6377607fa
ulfthella <ulfthe.lla@gmail.com>
2026-08-12 11:50:31 +0800

    Update stringLoncat.js

--- Learn JavaScript/stringLoncat.js
+++ Learn JavaScript/stringLoncat.js
@@ -1,6 +1,6 @@
 /* run time untuk melihat output kode */
 
-const myName = 'Dilla';
+const myName = 'kkk';
 const favFood = 'mie ayam';
 const description = 'hai nama saya ' + myName + ' dan makanan favorit saya adalah ' +  favFood;
 
